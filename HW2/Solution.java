fb11094679cc74c89944772a9c94db5f227eb5fd
Daniil <120733028+dkon0v@users.noreply.github.com>
2023-04-17 19:15:26 +0300

    Add files via upload

---
+++ HW2/Solution.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+    public String reverseWords(String s) {
+        List<String> words = Arrays.asList(s.trim().split("\\s+"));
+        Collections.reverse(words);
+        return String.join(" ", words);
+    }
+}
